e8b1296118e8721f1a5a1d78575f28c37f523f4d
belaya04062007arina-bit <belaya04062007arina@gmail.com>
2025-10-14 16:04:25 +0300

    Add files via upload

---
+++ lab2 -2choice/task 6.py
@@ -0,0 +1,3 @@
+strings=["Угол", "Ловушка", "Чип", "Банан"]
+sort= sorted(strings, key=len)
+print (sort)
---
+++ lab2 -2choice/task 8.py
@@ -0,0 +1,5 @@
+n= int(input())
+umn=[]
+
+for i in range(1, n+1):
+    print(f"{n}*{i}= {n*i}") 
